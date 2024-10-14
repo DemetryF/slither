20343956488cc439be4d5139d067998508acb897
demetry <djimabebeshko@gmail.com>
2024-10-14 21:11:18 +0300

    backend: add ability to specify port through `--port` flag

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,14 +3,20 @@ mod listener;
 mod state_updater;
 mod transfer;
 
+use std::env;
+use std::net::{Ipv4Addr, SocketAddr};
+use std::process::exit;
+
+use core::{GameState, World};
+
 use listener::Listener;
 use state_updater::StateUpdater;
 use tokio::sync::{broadcast, mpsc};
 
-use core::{GameState, World};
-
 #[tokio::main]
 async fn main() {
+    let port = port();
+
     let (connections_tx, connections_rx) = mpsc::channel(1);
     let (directions_tx, directions_rx) = mpsc::channel(16);
     let (crash_tx, crash_rx) = broadcast::channel(16);
@@ -25,11 +31,38 @@ async fn main() {
         .start(),
     );
 
+    let ip = Ipv4Addr::new(0, 0, 0, 0);
+    let addr = SocketAddr::new(ip.into(), port);
+
+    println!("start on {addr}");
+
     let listener = tokio::spawn(
-        Listener::start_on("192.168.0.11:1488", connections_tx, directions_tx, crash_rx)
+        Listener::start_on(addr, connections_tx, directions_tx, crash_rx)
             .await
             .listen(),
     );
 
     let _ = tokio::join!(updater, listener);
+}
+
+fn port() -> u16 {
+    let mut args = env::args();
+
+    while let Some(arg) = args.next() {
+        if arg == "--port" {
+            let Some(port) = args.next() else {
+                eprintln!("you must specify a port after \"--port\"");
+                exit(1);
+            };
+
+            let Ok(port) = u16::from_str_radix(&port, 10) else {
+                eprintln!("invalid port: \"{}\"", &port);
+                exit(1);
+            };
+
+            return port;
+        }
+    }
+
+    return 0;
 }
