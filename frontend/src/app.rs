6ff7ae8309094e1a885b63f11b2971598494b359
demetry <djimabebeshko@gmail.com>
2024-10-14 19:49:13 +0300

    frontend: refactor

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,12 +1,12 @@
 use std::net::{SocketAddr, TcpStream};
 use std::str::FromStr;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{self, AtomicBool};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
 use egui::emath::TSTransform;
-use egui::{Align, CentralPanel, Color32, Frame, Margin, Pos2, Sense, TextEdit};
+use egui::{Align, CentralPanel, Color32, Margin, Pos2, Sense, TextEdit};
 
 use core::{SlitherID, World};
 
@@ -14,160 +14,185 @@ use crate::painter::Painter;
 use crate::transfer::{SyncReceive, SyncSend};
 
 pub enum App {
-    Launcher {
-        server_ip: String,
-        nickname: String,
-        color: Color32,
-    },
-    Game {
-        state: State,
-        self_id: SlitherID,
-        world_size: Pos2,
-        transform: TSTransform,
-        last_dir_updation: Instant,
-    },
+    Launcher(Launcher),
+    Game(Game),
     None,
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         match self {
-            App::Launcher {
-                server_ip,
-                nickname,
-                color,
-            } => {
-                let mut join = false;
+            App::Launcher(launcher) => {
+                launcher.update(ctx);
 
-                CentralPanel::default().show(ctx, |ui| {
-                    ui.vertical_centered(|ui| {
-                        ui.add(
-                            TextEdit::singleline(nickname)
-                                .horizontal_align(Align::Center)
-                                .hint_text("nickname"),
-                        );
-
-                        ui.add(
-                            TextEdit::singleline(server_ip)
-                                .horizontal_align(Align::Center)
-                                .hint_text("server ip"),
-                        );
-
-                        ui.color_edit_button_srgba(color);
-
-                        join = ui.button("join").clicked();
-                    })
-                });
-
-                if join {
-                    // TODO: check ip and nickname
-                    let Self::Launcher {
-                        server_ip,
-                        nickname,
-                        color,
-                    } = std::mem::replace(self, Self::None)
-                    else {
-                        unreachable!();
+                if launcher.join_clicked {
+                    let App::Launcher(launcher) = std::mem::replace(self, Self::None) else {
+                        unreachable!()
                     };
 
-                    let Ok(addr) = SocketAddr::from_str(&server_ip) else {
-                        return;
-                    };
-
-                    let mut socket = TcpStream::connect(addr).unwrap();
-
-                    let mut buffer = Vec::new();
-
-                    protocol::PlayerJoin {
-                        color: Some(color),
-                        nickname,
+                    match launcher.try_start() {
+                        Ok(game) => *self = Self::Game(game),
+                        Err(launcher) => *self = Self::Launcher(launcher),
                     }
-                    .send(&mut buffer, &mut socket);
-
-                    let protocol::SessionStart {
-                        world_size,
-                        self_id,
-                    } = protocol::SessionStart::receive(&mut buffer, &mut socket);
-
-                    let state = State::new(socket);
-
-                    state.clone().receive();
-
-                    *self = Self::Game {
-                        state,
-                        self_id,
-                        world_size,
-                        transform: Default::default(),
-                        last_dir_updation: Instant::now(),
-                    };
-
-                    return;
                 }
             }
 
-            &mut Self::Game {
-                ref mut state,
-                self_id,
-                ref mut transform,
-                ref mut last_dir_updation,
-                ..
-            } => {
-                ctx.request_repaint();
-
-                if !state.is_game_over() {
-                    let head_pos = state.world(|world| world.slithers.get(self_id).body.head());
-                    let screen_center = ctx.screen_rect().size() / 2.0;
-
-                    transform.translation = -head_pos.to_vec2() + screen_center;
-
-                    if (Instant::now() - *last_dir_updation) > Duration::from_millis(100) {
-                        let mouse_pos = ctx.input(|i| i.pointer.hover_pos());
-
-                        if let Some(mouse_pos) = mouse_pos {
-                            *last_dir_updation = Instant::now();
-
-                            let virtual_mouse_pos = transform.inverse() * mouse_pos;
-
-                            let new_dir = (virtual_mouse_pos - head_pos).angle();
-
-                            state.change_dir(new_dir);
-                        }
-                    }
-                }
-
-                egui::CentralPanel::default()
-                    .frame(Frame {
-                        inner_margin: Margin::ZERO,
-                        outer_margin: Margin::ZERO,
-                        ..Default::default()
-                    })
-                    .show(ctx, |ui| {
-                        let (_, painter) =
-                            ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
-
-                        let painter = Painter {
-                            raw: painter,
-                            transform: *transform,
-                        };
-
-                        state.world(|world| {
-                            for clot in world.clots.iter() {
-                                let color = clot.color.linear_multiply(0.3);
-
-                                painter.circle(clot.pos, clot.radius(), color);
-                            }
-
-                            for (_, slither) in world.slithers.iter() {
-                                for &cell in slither.body.cells() {
-                                    painter.circle(cell, slither.body.cell_radius(), slither.color);
-                                }
-                            }
-                        });
-                    });
+            App::Game(game) => {
+                game.update(ctx);
             }
 
-            Self::None => {}
+            App::None => unreachable!(),
         }
+    }
+}
+
+#[derive(Default)]
+pub struct Launcher {
+    server_ip: String,
+    nickname: String,
+    color: Color32,
+    join_clicked: bool,
+}
+
+impl Launcher {
+    pub fn update(&mut self, ctx: &egui::Context) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.add(
+                    TextEdit::singleline(&mut self.nickname)
+                        .horizontal_align(Align::Center)
+                        .hint_text("nickname"),
+                );
+
+                ui.add(
+                    TextEdit::singleline(&mut self.server_ip)
+                        .horizontal_align(Align::Center)
+                        .hint_text("server ip"),
+                );
+
+                ui.color_edit_button_srgba(&mut self.color);
+
+                self.join_clicked = ui.button("join").clicked();
+            })
+        });
+    }
+
+    pub fn try_start(self) -> Result<Game, Launcher> {
+        let Ok(addr) = SocketAddr::from_str(&self.server_ip) else {
+            return Err(self);
+        };
+
+        if self.nickname.is_empty() {
+            return Err(self);
+        }
+
+        let Ok(mut listener) = TcpStream::connect(addr) else {
+            return Err(self);
+        };
+
+        let mut buffer = Vec::new();
+
+        protocol::PlayerJoin {
+            color: Some(self.color),
+            nickname: self.nickname,
+        }
+        .send(&mut buffer, &mut listener);
+
+        let start = protocol::SessionStart::receive(&mut buffer, &mut listener);
+
+        let state = State::new(listener);
+
+        state.clone().receive();
+
+        Ok(Game {
+            state,
+            self_id: start.self_id,
+            transform: TSTransform::IDENTITY,
+            last_dir_upd: Instant::now(),
+            world_size: start.world_size,
+        })
+    }
+}
+
+pub struct Game {
+    pub state: State,
+    pub self_id: SlitherID,
+    pub transform: TSTransform,
+    pub last_dir_upd: Instant,
+    pub world_size: Pos2,
+}
+
+impl Game {
+    pub fn update(&mut self, ctx: &egui::Context) {
+        ctx.request_repaint();
+
+        if !self.state.is_game_over() {
+            let screen_center = ctx.screen_rect().size() / 2.0;
+
+            self.transform.translation = -self.head_pos().to_vec2() + screen_center;
+
+            if self.can_update_dir() {
+                self.update_dir(ctx);
+            }
+        }
+
+        Self::panel().show(ctx, |ui| {
+            let (_, painter) = ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
+
+            let painter = Painter {
+                raw: painter,
+                transform: self.transform,
+            };
+
+            self.draw(&painter);
+        });
+    }
+
+    fn can_update_dir(&self) -> bool {
+        self.last_dir_upd + Duration::from_millis(500) > Instant::now()
+    }
+
+    fn update_dir(&mut self, ctx: &egui::Context) {
+        self.last_dir_upd = Instant::now();
+
+        let mouse_pos = ctx.input(|i| i.pointer.hover_pos());
+
+        if let Some(mouse_pos) = mouse_pos {
+            let virtual_mouse_pos = self.transform.inverse() * mouse_pos;
+
+            let dir = (virtual_mouse_pos - self.head_pos()).angle();
+
+            self.state.change_dir(dir);
+        }
+    }
+
+    fn head_pos(&self) -> Pos2 {
+        self.state
+            .world(|world| world.slithers.get(self.self_id).body.head())
+    }
+
+    fn draw(&self, painter: &Painter) {
+        self.state.world(|world| {
+            for clot in world.clots.iter() {
+                let color = clot.color.linear_multiply(0.3);
+
+                painter.circle(clot.pos, clot.radius(), color);
+            }
+
+            for (_, slither) in world.slithers.iter() {
+                for &cell in slither.body.cells() {
+                    painter.circle(cell, slither.body.cell_radius(), slither.color);
+                }
+            }
+        });
+    }
+
+    fn panel() -> egui::CentralPanel {
+        egui::CentralPanel::default().frame(egui::Frame {
+            inner_margin: Margin::ZERO,
+            outer_margin: Margin::ZERO,
+            ..Default::default()
+        })
     }
 }
 
@@ -194,25 +219,23 @@ impl State {
 
     pub fn receive(self) {
         thread::spawn(move || loop {
-            let info = bincode::deserialize_from(&mut *self.socket.lock().unwrap()).unwrap();
+            let info = self.socket(|socket| bincode::deserialize_from(socket).unwrap());
 
             match info {
                 protocol::ServerUpdate::GameOver => {
-                    self.game_over
-                        .store(true, std::sync::atomic::Ordering::Relaxed);
+                    self.game_over.store(true, atomic::Ordering::Relaxed);
                 }
 
                 protocol::ServerUpdate::PlayersTop => {
-                    let top = bincode::deserialize_from(&mut *self.socket.lock().unwrap()).unwrap();
+                    let top = self.socket(|socket| bincode::deserialize_from(socket).unwrap());
 
                     *self.top.lock().unwrap() = top;
                 }
 
                 protocol::ServerUpdate::World => {
-                    let new_world =
-                        bincode::deserialize_from(&mut *self.socket.lock().unwrap()).unwrap();
+                    let world = self.socket(|socket| bincode::deserialize_from(socket).unwrap());
 
-                    *self.world.lock().unwrap() = new_world;
+                    *self.world.lock().unwrap() = world;
                 }
             }
         });
@@ -227,7 +250,11 @@ impl State {
         f(&self.world.lock().unwrap())
     }
 
+    pub fn socket<R>(&self, f: impl FnOnce(&mut TcpStream) -> R) -> R {
+        f(&mut self.socket.lock().unwrap())
+    }
+
     pub fn is_game_over(&self) -> bool {
-        self.game_over.load(std::sync::atomic::Ordering::Relaxed)
+        self.game_over.load(atomic::Ordering::Relaxed)
     }
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,21 +3,14 @@ mod painter;
 mod transfer;
 
 use eframe::NativeOptions;
-use egui::Color32;
 
-use app::App;
+use app::{App, Launcher};
 
 fn main() {
     eframe::run_native(
         "slither",
         NativeOptions::default(),
-        Box::new(|_| {
-            Ok(Box::new(App::Launcher {
-                server_ip: "".into(),
-                nickname: "".into(),
-                color: Color32::WHITE,
-            }))
-        }),
+        Box::new(|_| Ok(Box::new(App::Launcher(Launcher::default())))),
     )
     .unwrap();
 }
